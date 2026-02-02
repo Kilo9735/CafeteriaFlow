9a7d90f202bbd3df8545ba0c3becd3790796d3f2
trrb <tesakov.ruslan@gmail.com>
2026-02-02 14:56:55 +0300

    начало работать хтмл

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dataalchemy.db_session import create_session, global_init
 from dataalchemy.models import User, Role
 
@@ -7,35 +7,13 @@ app.config['SECRET_KEY'] = 'crewdestruct'
 
 global_init()
 
-@app.route("/test_db")
-def test_db():
-    # создаём сессию для этого запроса
-    db = create_session()
-    try:
-        # Проверяем роль
-        role = db.query(Role).filter(Role.name == "Student").first()
-        if not role:
-            role = Role(name="Student")
-            db.add(role)
-            db.commit()  # commit чтобы role.id появился
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('Log_in.html')
 
-        # Проверяем пользователя
-        user = db.query(User).filter(User.email == "ivan@example.com").first()
-        if not user:
-            user = User(email="ivan@example.com", password="12345", balance=100.0, role=role)
-            db.add(user)
-            db.commit()  # commit сохраняет пользователя
-
-        # Выводим всех пользователей
-        users = db.query(User).all()
-        result = "<br>".join([f"{u.email} — {u.role.name} — {u.balance}" for u in users])
-        return f"<h3>Пользователи в базе:</h3>{result}"
-
-    except Exception as e:
-        db.rollback()  # если ошибка, откат
-        return f"Ошибка: {e}"
-    finally:
-        db.close()  # закрываем сессию
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('registration.html')
 
 
 
