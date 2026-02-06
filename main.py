ad0b2140976f5941ca720dbdf21b80f12209f9df
Kilo9735 <kilo9735@gmail.com>
2026-02-07 02:09:44 +0300

    Вывел сумму всего заказа

--- main.py
+++ main.py
@@ -178,14 +178,15 @@ def new_reviews():
 
 
 @app.route('/bascket', methods=['GET', 'POST'])
-def bascket():#test
+def bascket():
     form = BascketForm()
     session = create_session()
     object = session.query(Bascket).filter(Bascket.user_id == current_user.id).all()
     breakfast = session.query(Dish).filter(Dish.type == 'breakfast' and Bascket.user_id == current_user.id).all()
     lunch = session.query(Dish).filter(Dish.type == 'lunch' and Bascket.user_id == current_user.id).all()
     dishes = session.query(Dish).filter(Dish.type == 'dish' and Bascket.user_id == current_user.id).all()
-
+    bascket_sum = sum(elem.dish.price for elem in object)
+    print(bascket_sum)
     if form.validate_on_submit():
         if form.profile.data:
             return redirect(url_for('profile'))
@@ -195,7 +196,7 @@ def bascket():#test
             return redirect(url_for('reviews'))
         elif form.top_up_acc.data:
             return redirect(url_for('top_up_acc'))
-    return render_template('bascket.html', form=form, object=object, dishes=dishes, breakfast=breakfast, lunch=lunch)
+    return render_template('bascket.html', form=form, object=object, dishes=dishes, breakfast=breakfast, lunch=lunch, bascket_sum=bascket_sum)
 
 
 @app.route('/top_up_acc', methods=['GET', 'POST'])
